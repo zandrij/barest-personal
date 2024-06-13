9080ae9fb2e4c7072c90533443986664a0b8ea8e
zandrij <ajosuesh@gmail.com>
2024-06-12 20:43:45 -0400

    feat: menu options external

---
+++ src/presentation/utils/MenuOptions.tsx
@@ -0,0 +1,50 @@
+import MarketplaceIcon from "./MarketplaceIcon";
+import JobsIcon from "./JobsIcon";
+import BusinessIcon from "./BusinessIcon";
+import { PiHouse } from 'react-icons/pi';
+import { CiDeliveryTruck } from "react-icons/ci";
+import { IoChatboxEllipsesOutline, IoPersonOutline } from "react-icons/io5";
+
+interface Options {
+    name: string;
+    icon: any;
+    url: string;
+}
+
+export const menuOptions: Options[] = [
+    {
+        name: "Inicio",
+        icon: PiHouse,
+        url: "/home",
+    },
+    {
+        name: "Marketplace",
+        icon: MarketplaceIcon,
+        url: "/marketplace",
+    },
+    {
+        name: "Empleos",
+        icon: JobsIcon,
+        url: "/jobs",
+    },
+    {
+        name: "Negocios",
+        icon: BusinessIcon,
+        url: "/business",
+    },
+    {
+        name: "Distribuidores",
+        icon: CiDeliveryTruck,
+        url: "/distributors",
+    },
+    {
+        name: "Mensajes",
+        icon: IoChatboxEllipsesOutline,
+        url: "/messages",
+    },
+    {
+        name: "Perfil",
+        icon: IoPersonOutline,
+        url: "/profile",
+    },
+];
